094aac23ff6f26da5a458fc3a8a540556fbd381b
unknown <aymankacemi01@gmail.com>
2023-09-08 18:00:00 +0100

    Connect to mongoDB

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');  // in the mongoURI you can't have special character in name or password so  for my case i used %40 instead of @
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try{
+       await mongoose.connect(db,{
+        useNewUrlParser:true,
+       });
+       console.log('MongoDB Connected...');
+    }catch(err){
+        console.error(err.message);
+        //Exit process with failure
+        process.exit(1);
+    }
+}
+module.exports = connectDB;
