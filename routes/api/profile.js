26ff9d02ac4fa03458cfc17e0823f5b052269b68
unknown <aymankacemi01@gmail.com>
2023-09-12 09:55:47 +0100

    get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,11 +12,13 @@ const User = require('../../models/User');
 
 router.get('/me', auth, async (req, res) => {
     try{
+    
         const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name','avatar']);
-
         if(!profile){
             return res.status(400).json({msg: 'There is no profile for this user'})
         }
+        res.json(profile);
+        
     }catch(err){
         console.log(err.message);
         res.status(500).send('Server Error');
@@ -88,6 +90,21 @@ async (req, res) =>{
     }catch(err){
         console.log(err.message);
         res.status(500).send('Server Error');
+    }
+});
+
+// @route GET api/profile
+// @desc    get all profiles 
+// @access Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']); // populate('collectionName',['field1','Field2', ....])
+        res.json(profiles);
+        
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+       
     }
 });
 module.exports  = router;
