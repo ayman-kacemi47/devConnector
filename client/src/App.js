b0e685a59a398cf2ebdbe168f19359b84d66be45
unknown <aymankacemi01@gmail.com>
2023-09-24 11:20:27 +0100

    get started with Redux, creating alert reducer , action

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,24 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
+// Redux ( to be able to use that store ...)
+import { Provider } from 'react-redux';
+import store from './store';
 
-        <Route exact path='/register' element={<Register />}></Route>
-        <Route exact path='/login' element={<Login />}></Route>
-      </Routes>
-    </Fragment>
-  </Router>
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+
+          <Route exact path='/register' element={<Register />}></Route>
+          <Route exact path='/login' element={<Login />}></Route>
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ client/src/reducers/alert.js
@@ -0,0 +1,22 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = [
+  /*  the intial State is an empty array , this is exemple :
+    {
+        id: 1,
+        msg:'Please Log in',
+        alertType : 'success'   for colors 
+    }
+     */
+];
+export default function (state = initialState, action) {
+  const { type, payload } = action; // to avoid action.type & action.payload
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+}
