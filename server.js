a26dfaa18c44b385f64725fd2a9113d4847bdce4
ayman kacemi <aymankacemi01@gmail.com>
2023-10-30 11:04:10 +0100

    prepare to deploy

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect database
 connectDB();
 
 //Init middleware
-app.use(express.json({ extended : false}))  // this should allow us to use req.body  , mohema
+app.use(express.json({ extended: false })); // this should allow us to use req.body  , mohema
 
-app.get('/' , (req, res) => res.send('API Running'));
+//app.get('/' , (req, res) => res.send('API Running'));   deleted for deploying
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +17,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 5000 ;
+//Serve static assets in production
+if (process.env.NOD_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
